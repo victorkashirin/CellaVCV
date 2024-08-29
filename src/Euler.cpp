a39509632b9cf59d6a6962f3dd3ca55832b9ea87
Victor Kashirin <victor@contextlabs.com>
2024-08-29 22:03:58 +0200

    Update Euler with smoothing and additional outputs

--- src/Euler.cpp
+++ src/Euler.cpp
@@ -4,6 +4,7 @@
 struct Euler : Module {
     enum ParamId {
         FREQ_PARAM,
+        SMOOTH_PARAM,
         PARAMS_LEN
     };
     enum InputId {
@@ -12,6 +13,9 @@ struct Euler : Module {
     };
     enum OutputId {
         SLOPE_OUTPUT,
+        SLOPE_ABS_OUTPUT,
+        SLOPE_POS_OUTPUT,
+        SLOPE_NEG_OUTPUT,
         OUTPUTS_LEN
     };
     enum LightId {
@@ -23,20 +27,57 @@ struct Euler : Module {
     double previousVoltage = 0.f;
     double currentValue = 0.f;
     int step = 0;
+    int smoothStep = 0;
     dsp::ClockDivider lightDivider;
+    float *steps;
+    float average = 0.f;
+    int _sampleRate;
+
+    float calculateMovingAverage(const float *steps, float average, int size, int step, int windowLength) {
+        return average + (steps[step % size] - steps[((step - windowLength + 1 + size) % size)]) / windowLength;
+    }
+
+    float pos(float signal) {
+        if (signal > 0.f) return signal;
+        return 0.f;
+    }
+
+    float neg(float signal) {
+        if (signal < 0.f) return -signal;
+        return 0.f;
+    }
 
     Euler() {
         config(PARAMS_LEN, INPUTS_LEN, OUTPUTS_LEN, LIGHTS_LEN);
         configParam(FREQ_PARAM, -8.f, 4.f, 1.f, "Frequency", " Hz", 2, 1);
+        configParam(SMOOTH_PARAM, 0.f, 1.f, 0.f, "Smooth", " Seconds");
         configInput(SIGNAL_INPUT, "Sampling signal");
         configOutput(SLOPE_OUTPUT, "Normalised angle of the slope");
         lightDivider.setDivision(16);
+        _sampleRate = (int)APP->engine->getSampleRate();
+        steps = new float[_sampleRate];
+    }
+
+    void onSampleRateChange() override {
+        delete[] steps;
+        _sampleRate = (int)APP->engine->getSampleRate();
+        steps = new float[_sampleRate];
+        average = 0.f;
+        smoothStep = 0;
+        step = 0;
     }
 
     void process(const ProcessArgs &args) override {
         double voltage = inputs[SIGNAL_INPUT].getVoltage();
         double pitch = params[FREQ_PARAM].getValue();
+        double smooth = params[SMOOTH_PARAM].getValue();
         double freq = std::pow(2.f, pitch);
+
+        steps[smoothStep] = voltage;
+        if (smooth * _sampleRate >= 1.f) {
+            average = calculateMovingAverage(steps, average, _sampleRate, smoothStep, (int)(smooth * _sampleRate));
+            voltage = average;
+        }
 
         // sampling window affects precision on low frequencies
         int window = (int)clamp(2.f / freq, 1, 1024);
@@ -45,14 +86,19 @@ struct Euler : Module {
             double diff = voltage - previousVoltage;
             double angle = std::atan2(diff, window * args.sampleTime * 31.5 * freq) * 180 / M_PI;
             currentValue = 10.f * angle / 90.f;
-            outputs[SLOPE_OUTPUT].setVoltage(currentValue);
             previousVoltage = voltage;
-        } else {
-            outputs[SLOPE_OUTPUT].setVoltage(currentValue);
         }
+
+        outputs[SLOPE_OUTPUT].setVoltage(currentValue);
+        outputs[SLOPE_ABS_OUTPUT].setVoltage(std::abs(currentValue));
+        outputs[SLOPE_POS_OUTPUT].setVoltage(pos(currentValue));
+        outputs[SLOPE_NEG_OUTPUT].setVoltage(neg(currentValue));
 
         step += 1;
         step = step % window;
+
+        smoothStep += 1;
+        smoothStep = smoothStep % _sampleRate;
 
         // LIGHT
         if (lightDivider.process()) {
@@ -71,13 +117,17 @@ struct EulerWidget : ModuleWidget {
         addChild(createWidget<ScrewGrey>(Vec(0, 0)));
         addChild(createWidget<ScrewGrey>(Vec(0, RACK_GRID_HEIGHT - RACK_GRID_WIDTH)));
 
-        addChild(createLightCentered<LargeFresnelLight<GreenRedLight>>(Vec(30, 35), module, Euler::SIG_LIGHT_POS));
+        addChild(createLightCentered<LargeFresnelLight<GreenRedLight>>(Vec(37.5, 35), module, Euler::SIG_LIGHT_POS));
 
-        addParam(createParamCentered<RoundBlackKnob>(Vec(30, 104.36), module, Euler::FREQ_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(Vec(37.5, 104.36), module, Euler::FREQ_PARAM));
+        addParam(createParamCentered<RoundBlackKnob>(Vec(37.5, 154.33), module, Euler::SMOOTH_PARAM));
 
-        addInput(createInputCentered<PJ301MPort>(Vec(30, 231.9), module, Euler::SIGNAL_INPUT));
+        addInput(createInputCentered<PJ301MPort>(Vec(37.5, 231.9), module, Euler::SIGNAL_INPUT));
 
-        addOutput(createOutputCentered<PJ301MPort>(Vec(30, 330.01), module, Euler::SLOPE_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(Vec(21, 280.01), module, Euler::SLOPE_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(Vec(54, 280.01), module, Euler::SLOPE_ABS_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(Vec(21, 330.01), module, Euler::SLOPE_POS_OUTPUT));
+        addOutput(createOutputCentered<PJ301MPort>(Vec(54, 330.01), module, Euler::SLOPE_NEG_OUTPUT));
     }
 };
 
